95de639e22fb135058baaaa07249fbd0553469c2
Billy Irwin <birwin93@gmail.com>
2020-07-19 12:24:53 -0700

    Enables swiftlint in dangerfile (#3)
    * Enables swiftlint in dangerfile
    
    * dafsd
    
    * dfasd
    
    * dfasd
    
    * dafds

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -4,3 +4,12 @@ let danger = Danger()
 if danger.git.createdFiles.count > 1 {
     warn("Please add tests if you've created more than one file")
 }
+
+SwiftLint.lint(
+    .modifiedAndCreatedFiles(directory: nil),
+    inline: true,
+    configFile: ".swiftlint.yml",
+    strict: true,
+    quiet: false,
+    swiftlintPath: danger.utils.exec("mint which swiftlint")
+)
